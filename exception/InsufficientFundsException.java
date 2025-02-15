c34d58dcaaf31e3945c95405e02a8a36ac4e8696
EHizon99 <hizon.andre26@gmail.com>
2025-02-15 20:48:48 +0800

    Delete exception directory

--- exception/InsufficientFundsException.java
+++
@@ -1,7 +0,0 @@
-package com.training.week1.exception;
-
-public class InsufficientFundsException extends Exception{
-    public InsufficientFundsException(String message) {
-        super(message);
-    }
-}
